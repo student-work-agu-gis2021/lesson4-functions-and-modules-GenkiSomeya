e30e8276e3820b1fb19a51d97d07fe1d02fffb15
GenkiSomeya <>
2021-05-09 14:01:25 +0000

    I added a function and print.

--- Exercise_4_problem_2.py
+++ Exercise_4_problem_2.py
@@ -25,17 +25,27 @@
 
 
 # YOUR CODE HERE to define temp_classifier
+def temp_classifier(temp_celsius):
 
 # Use your newly created function to print the answers to the following questions:
+  if(temp_celsius<-2):
+    return 0
+  elif(-2<=temp_celsius<2):
+    return 1
+  elif(2<=temp_celsius<15):
+    return 2
+  elif(temp_celsius>=15):
+    return 3
 
 
 # 1. What is the class value for 16.5 degrees Celsius?
 # YOUR CODE HERE
+print(temp_classifier(16.5))
 #
 # 2. What is the class value for +2 degrees Celsius?
 # YOUR CODE HERE
 #
-
+print(temp_classifier(2))
 
 # ### Problem 2 tests
 # 
